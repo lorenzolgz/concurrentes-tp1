024c6f9f7d0f8edb07ac1975f375a1d86d3e6416
lzogimenez <lgimenez@fi.uba.ar>
2021-10-05 21:16:38 -0300

    Use for loop to instance semaphores per airline

--- src/record_manager_factory.rs
+++ src/record_manager_factory.rs
@@ -9,24 +9,19 @@ pub struct RecordManagerFactory {
 
 impl RecordManagerFactory {
     pub fn new() -> RecordManagerFactory {
-        let parallel_requests_count = 5;
 
+        let parallel_requests_count = 5;
+        let airlines = vec!["AERO_1", "AERO_2", "AERO_3"];
         let mut airline_to_semaphore = HashMap::new();
-        airline_to_semaphore.insert(
-            "AERO_1".to_string(),
-            Arc::new(Semaphore::new(parallel_requests_count))
-        );
-        airline_to_semaphore.insert(
-            "AERO_2".to_string(),
-            Arc::new(Semaphore::new(parallel_requests_count))
-        );
-        airline_to_semaphore.insert(
-            "AERO_3".to_string(),
-            Arc::new(Semaphore::new(parallel_requests_count))
-        );
-        RecordManagerFactory {
-            airline_to_semaphore
+
+        for airline in airlines {
+            airline_to_semaphore.insert(
+                airline.to_string(),
+                Arc::new(Semaphore::new(parallel_requests_count))
+            );
         }
+
+        RecordManagerFactory { airline_to_semaphore }
     }
 
     pub fn get_manager(&self, record: Arc<Record>) -> Option<RecordManager> {
